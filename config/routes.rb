796a9c624d598db741b11d4a54ca7d7524cb4ee1
matthieu delegrange <mdelegrange@gmail.com>
2018-11-26 17:29:50 +0100

    Added all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cats, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:index, :show]
+
+  namespace :owner do
+    resources :cats, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :bookings, only: [:index, :show] do
+      member do
+        patch :accept
+        patch :deny
+      end
+    end
+  end
+
+  resource :profile, only: [:show, :edit, :update]
 end
