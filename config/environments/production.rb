35cd31d2dc7a835fe7312dfac9c000d80fe19faa
matthieu delegrange <mdelegrange@gmail.com>
2018-11-26 16:20:43 +0100

    Devise setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "catsnb_#{Rails.env}"
 
+  config.action_mailer.default_url_options = { host: 'catsnb.cleverapps.io' }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
