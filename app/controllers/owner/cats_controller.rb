68eb242272c6f84b54683e84098d083588336a7f
matthieu delegrange <mdelegrange@gmail.com>
2018-11-27 11:21:28 +0100

    Add cat adding form

--- app/controllers/owner/cats_controller.rb
+++ app/controllers/owner/cats_controller.rb
@@ -1,4 +1,24 @@
 class Owner::CatsController < ApplicationController
   def new
+    @cat = Cat.new
   end
+
+  def create
+    @cat = Cat.new(cat_params)
+    @user = current_user
+    @cat.user = @user
+
+    if @cat.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :breed, :description, :birth_date, :photo, :price_per_day)
+  end
+
 end
