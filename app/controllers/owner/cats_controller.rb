613dc1c0fb083d276d6cbe442088dc1480c40114
matthieu delegrange <mdelegrange@gmail.com>
2018-11-27 15:07:18 +0100

    Edit Update Cat info

--- app/controllers/owner/cats_controller.rb
+++ app/controllers/owner/cats_controller.rb
@@ -1,4 +1,6 @@
 class Owner::CatsController < ApplicationController
+  before_action :set_cat, only: [:edit, :update]
+
   def new
     @cat = Cat.new
   end
@@ -19,17 +21,21 @@ class Owner::CatsController < ApplicationController
   end
 
   def update
-    @cat.update(cat_params)
-    if @cat.save
-      redirect_to owner_cat_path(@cat)
+    if @cat.update(cat_params)
+      redirect_to owner_cats_path
     else
-      render :new
+      render :edit
+    end
   end
 
   private
 
   def cat_params
     params.require(:cat).permit(:name, :breed, :description, :birth_date, :photo, :price_per_day)
+  end
+
+  def set_cat
+    @cat = Cat.find(params[:id])
   end
 
 end
