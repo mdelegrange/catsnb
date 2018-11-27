b5a8eef5f1f1a8323214a24eeea40219e4347e10
yassine jay <yassinejay@hotmail.fr>
2018-11-27 17:49:10 +0100

    owner booking accept deny done

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,12 +1,24 @@
 class Owner::BookingsController < ApplicationController
-
+	before_action :set_booking, only: [:index, :accept, :deny]
 	def index
-		@bookings = current_user.received_bookings
 	end
 
 	def accept
+		@booking = @bookings.find(params[:id])
+		@booking.update(status: "accepted")
+		redirect_to owner_bookings_path
 	end
 
 	def deny
+		@booking = @bookings.find(params[:id])
+		@booking.update(status: "denied")
+		redirect_to owner_bookings_path
 	end
+
+	private
+
+	def set_booking
+		@bookings = current_user.received_bookings
+	end
+ 
 end
