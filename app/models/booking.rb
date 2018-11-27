7b94188621929858bb8c034a0ccb6b48295d30bb
yassine jay <yassinejay@hotmail.fr>
2018-11-27 16:29:36 +0100

    ownerbook modified

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :cat
   belongs_to :user
 
-  validates :status, inclusion: { in: ['inprogress', 'accepted', 'denied'] }
+  validates :status, inclusion: { in: ['pending', 'accepted', 'denied'] }
   validates :begin_date, presence: true
   validates :end_date, presence: true
 end
