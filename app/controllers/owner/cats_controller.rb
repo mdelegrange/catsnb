e6cf55cb9414a5d2d9f8505d7e8148819904d35d
matthieu delegrange <mdelegrange@gmail.com>
2018-11-27 13:52:07 +0100

    Fixed update bug

--- app/controllers/owner/cats_controller.rb
+++ app/controllers/owner/cats_controller.rb
@@ -1,4 +1,5 @@
 class Owner::CatsController < ApplicationController
+  before_action :set_cat, only: [ :edit, :update ]
 
   def index
     @cats = current_user.cats
@@ -21,19 +22,22 @@ class Owner::CatsController < ApplicationController
   end
 
   def edit
-    @cat = Cat.find(params[:id])
   end
 
   def update
     @cat.update(cat_params)
     if @cat.save
-      redirect_to owner_cat_path(@cat)
+      redirect_to owner_cats_path
     else
       render :new
     end
   end
 
   private
+
+  def set_cat
+    @cat = Cat.find(params[:id])
+  end
 
   def cat_params
     params.require(:cat).permit(:name, :breed, :description, :birth_date, :photo, :price_per_day)
