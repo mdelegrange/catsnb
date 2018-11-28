18d7b919fca13236ac474e7026238641142ffd68
matthieu delegrange <mdelegrange@gmail.com>
2018-11-28 16:10:51 +0100

    Corrected bug in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,25 +25,15 @@ user_renter3 = User.create!(email:'renter3@renter.fr', password: 'password', fir
 puts "Users Created"
 
 cat1 = Cat.create!(user: matthieu, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: 'Châtelet les Halles, Paris', price_per_day: 15, photo: File.open(Rails.root.join('db/fixtures/cats/cat1.jpg')))
-sleep(1)
-cat2 = Cat.create!(user: matthieu, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: '2 rue du Général Lanrezac, Nantes', price_per_day: 10, photo: File.open(Rails.root.join('db/fixtures/cats/cat2.jpg')))
-sleep(1)
+cat2 = Cat.create!(user: matthieu, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: '2 rue Général Lanrezac, Nantes', price_per_day: 10, photo: File.open(Rails.root.join('db/fixtures/cats/cat2.jpg')))
 cat3 = Cat.create!(user: yassine, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: 'Bouffay, Nantes', price_per_day: 20, photo: File.open(Rails.root.join('db/fixtures/cats/cat3.jpg')))
-sleep(1)
 cat4 = Cat.create!(user: yassine, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: 'Angers', price_per_day: 15, photo: File.open(Rails.root.join('db/fixtures/cats/cat4.jpg')))
-sleep(1)
 cat5 = Cat.create!(user: valentin, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: 'Lyon', price_per_day: 17, photo: File.open(Rails.root.join('db/fixtures/cats/cat5.jpg')))
-sleep(1)
 cat6 = Cat.create!(user: valentin, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: 'Denfert Rochereau, Paris', price_per_day: 16, photo: File.open(Rails.root.join('db/fixtures/cats/cat6.jpg')))
-sleep(1)
 cat7 = Cat.create!(user: pierre, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: 'Saint Etienne', price_per_day: 14, photo: File.open(Rails.root.join('db/fixtures/cats/cat7.jpg')))
-sleep(1)
 cat8 = Cat.create!(user: pierre, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: 'Pornic', price_per_day: 12, photo: File.open(Rails.root.join('db/fixtures/cats/cat8.jpg')))
-sleep(1)
 cat9 = Cat.create!(user: valentin, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: 'Rezé', price_per_day: 19, photo: File.open(Rails.root.join('db/fixtures/cats/cat9.jpg')))
-sleep(1)
 cat10 = Cat.create!(user: matthieu, name: Faker::Cat.name, description: Faker::Cat.registry, breed: Faker::Cat.breed, address: '26 boulevard de Stalingrad, Nantes', price_per_day: 20, photo: File.open(Rails.root.join('db/fixtures/cats/cat10.jpg')))
-sleep(1)
 
 puts "Cats Created"
 
