b7effea6ee4a13e43b0dd354c53932b0a38f9647
valentin burgaud <burval3@gmail.com>
2018-11-28 12:38:33 +0100

    fix pick-data bug & show first ugly version of owner name and avatar on cat#show

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,5 @@
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
 
 flatpickr(".datepicker", {
   minDate: "today",
@@ -7,4 +7,3 @@ flatpickr(".datepicker", {
   altFormat: "F j, Y",
   dateFormat: "Y-m-d",
 })
-
