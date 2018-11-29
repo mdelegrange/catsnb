ce43ef49ec6415f5e41e8e7a3081da7b93147a9e
matthieu delegrange <mdelegrange@gmail.com>
2018-11-29 12:22:02 +0100

    Added Review model, validations and associations

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  belongs_to :booking
+
+  validates :description, presence: true
+  validates :rating,
+            presence: true,
+            numericality: { only_integer: true },
+            inclusion: { in: [0, 1, 2, 3, 4, 5] }
+end
